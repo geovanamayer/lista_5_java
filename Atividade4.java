0d81b7e257b53562f368bf6862601e6a39740d9f
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2023-03-23 14:10:26 -0300

    Add files via upload

---
+++ Atividade4.java
@@ -0,0 +1,28 @@
+public class Atividade4 {
+    public static void main(String[] args) {
+
+        int[] nums = { 1, 2, 3, 4, -4 };
+        int min, max;
+
+        min = max = nums[0];
+
+        for (int i = 1; i < 5; i++) {
+
+            if (nums[i] < min)
+                min = nums[i];
+
+            if (nums[i] > max)
+                max = nums[i];
+
+            for (int numero : nums)
+
+            {
+                System.out.print(numero + " ");
+
+            }
+        }
+
+        System.out.println("Min e Max: " + min + " " + max);
+    }
+
+}
