355c6767e22c27721f3e18cd783b03190877c8da
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2023-03-23 14:12:04 -0300

    Add files via upload

---
+++ SomaVarargs.java
@@ -0,0 +1,16 @@
+public class SomaVarargs {
+    public static int soma(int... numeros) {
+        int resultado = 0;
+        for (int numero : numeros) {
+            resultado += numero;
+        }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        int soma1 = soma(1, 2, 3, 4, 5);
+        int soma2 = soma(10, 20, 30);
+        System.out.println("Soma1: " + soma1);
+        System.out.println("Soma2: " + soma2);
+    }
+}
