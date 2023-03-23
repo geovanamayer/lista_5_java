e79366f5b2972a331c8de31f4b244ac590d61914
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2023-03-23 14:07:49 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[ ] args) {
+
+
+        int[ ] array = new int [5];
+   
+    for ( int i = 0; i < array.length; i++ )
+        array[i] =i +1;
+   
+   
+    System.out.printf("%s\n","Valor");
+        for ( int i = 0; i < array.length; i++)
+   
+    System.out.printf("%d\n", array[i]);
+   
+
+
+}
+
+}
